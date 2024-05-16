ff60b58ca7ef9e9b5b581b155f7d301603040c10
Pilar Kern <pilikern04@gmail.com>
2024-05-16 18:25:57 -0300

    hice el test de la función descifrar

--- tests.hs
+++ tests.hs
@@ -4,9 +4,21 @@ import Test.HUnit
 ejecutarCifrar = runTestTT testCifrar
 
 testCifrar = test [
-    "Caso 1: palabra es vacía" ~: (cifrar [] 5) ~?= []
-    "Caso 2: todas minúsculas" ~: (cifrar "computacion" 6) ~?= "iusvazgiout"
-    "Caso3: todas mayúsculas" ~: (cifrar "COMPUTACION" 9) ~?= "COMPUTACION"
+    "Caso 1: palabra es vacía" ~: (cifrar [] 5) ~?= [],
+    "Caso 2: todas minúsculas" ~: (cifrar "computacion" 6) ~?= "iusvazgiout",
+    "Caso3: todas mayúsculas" ~: (cifrar "COMPUTACION" 9) ~?= "COMPUTACION",
     "Caso 4: mayúsculas y minúsculas intercaladas" ~: (cifrar "comPuTAcioN" 17) ~?= "tfPlTAtzfN" 
  ]
 
+
+ejecutarDescifrar = runTestTT testDescifrar
+
+testDescifrar = test [
+    "Caso 1: palabra es vacía" ~: (descifrar [] 4) ~?= [],
+    "Caso 2: todas minúsculas" ~: (descifrar "iusvazgiout" 6) ~?= "computacion",
+    "Caso 3: todas mayúsculas" ~: (descifrar "COMPUTACION" 9) ~?= "COMPUTACION",
+    "Caso 4: mayúsculas y minúsculas intercaladas" ~: (descifrar "tfPlTAtzfN" 17) ~?= "comPuTAcioN"
+ ]
+
+
+ 
