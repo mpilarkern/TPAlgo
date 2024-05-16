c1265a39409814b546bf16521b900f233cf38535
Pilar Kern <pilikern04@gmail.com>
2024-05-16 18:03:16 -0300

    hice el test de la función cifrar

--- tests.hs
+++ tests.hs
@@ -1,3 +1,12 @@
 import Solucion
-import HUnit
+import Test.HUnit
+
+ejecutarCifrar = runTestTT testCifrar
+
+testCifrar = test [
+    "Caso 1: palabra es vacía" ~: (cifrar [] 5) ~?= []
+    "Caso 2: todas minúsculas" ~: (cifrar "computacion" 6) ~?= "iusvazgiout"
+    "Caso3: todas mayúsculas" ~: (cifrar "COMPUTACION" 9) ~?= "COMPUTACION"
+    "Caso 4: mayúsculas y minúsculas intercaladas" ~: (cifrar "comPuTAcioN" 17) ~?= "tfPlTAtzfN" 
+ ]
 
